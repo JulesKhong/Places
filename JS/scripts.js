acc01afee3bbf6b92ee3430549e1e9647dbb678c
Adam Luchini and Julia Dickey <julia@>
2016-06-13 10:34:11 -0700

    functional with correct things

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,17 +1,22 @@
-function Places (location, landmarks, timeOfYear, notes){
-  this.locations = location,
-  this.landmarks = landmarks,
-  this.timeOfYear = timeOfYear,
-  this.notes = notes,
+function Place (locations, landmarks, timeOfYear, notes){
+  this.locations = locations;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
+
+
+Place.prototype.locations = function(){
+  return this.locations.toUpperCase();
 }
 
 // TO RESET FIELDS
-function resetFields() {
-  $("input#locations").val("");
-  $("input#landmarks").val("");
-  $("input#timeOfYear").val("");
-  $("input#notes").val("");
-}
+// function resetFields() {
+//   $("input#locations").val("");
+//   $("input#landmarks").val("");
+//   $("input#timeOfYear").val("");
+//   $("input#notes").val("");
+// }
 
 // USER INTERFACE LOGIC
 
@@ -20,12 +25,25 @@ $(document).ready(function(){
   $("#newPlaces").submit(function(){
     event.preventDefault();
 
-    var locations = $("input#locations").val();
-    var landmarks = $("input#landmarks").val();
-    var timeOfYear = $("input#timeOfYear").val();
-    var notes = $("notes").val();
+    var locations = $("input#userLocations").val();
+    var landmarks = [$("input#userLandmarks").val()];
+    var timeOfYear = $("input#userTimeOfYear").val();
+    var notes = $("input#userNotes").val();
+    var newPlace = new Place(locations, landmarks, timeOfYear, notes);
 
-    $("ul#locationsList").append("<li><span class="local">" + Places.locations() + "</span></li>");
+    var newPlaceLocations = newPlace.locations;
 
-  }
-}
+
+    $("ul#locationsList").append("<li><span class='local'>" + newPlace.locations + "</span></li>");
+    console.log(newPlace.locations);
+
+    $("#locationsList").last().click(function() {
+      $("#results").show();
+      $(".locations").text(newPlace.locations);
+      $(".landmarks").text(newPlace.landmarks);
+      $(".timeOfYear").text(newPlace.timeOfYear);
+      $(".notes").text(newPlace.notes);
+    });
+
+  });
+});
